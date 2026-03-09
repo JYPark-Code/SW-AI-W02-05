439db4afdb385df76cb2c99c7c399aec2c23eeec
jypark <toboy93@gmail.com>
2026-03-09 09:39:06 +0900

    feat: 2가지 방법으로 구현, Counter 사용

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,24 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+from collections import Counter
+
+s = input().upper() # MISSISSIPPI
+c = Counter(s)
+
+# 횟수 세기
+# max_count = max(c.values())
+#
+# if list(c.values()).count(max_count) > 1:
+#     print("?")
+# else:
+#     for k, v in c.items():
+#         if v == max_count:
+#             print(k)
+
+# 최상위 2개만 뽑아내기 - better
+top = c.most_common(2) # [('I', 4), ('S', 4)]
+
+if len(top) > 1 and top[0][1] == top[1][1]:
+    print("?")
+else:
+    print(top[0][0])
