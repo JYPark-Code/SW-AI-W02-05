2a6eaa8881822243fe10a272567cc58fabd36a9c
jypark <toboy93@gmail.com>
2026-03-10 16:53:05 +0900

    feat: 대각선과 같은 열 조건문 O(n) 시간 -> O(1)으로 바꾸기,

--- week2/problem-solving/난이도중_백트래킹_NQueen_골드4.py
+++ week2/problem-solving/난이도중_백트래킹_NQueen_골드4.py
@@ -40,11 +40,9 @@ col = [-1] * n
 
 answer = 0
 
-def is_valid(row, c):
-    for r in range(row):
-        if col[r] == c or abs(col[r] - c) == abs(row - r):
-            return False
-    return True
+used_col = [False] * n
+used_diag1 = [False] * (2*n)
+used_diag2 = [False] * (2*n)
 
 def backtrack(row):
     global answer
@@ -53,9 +51,22 @@ def backtrack(row):
         return
 
     for c in range(n):
-        if is_valid(row, c):
-            col[row] = c
-            backtrack(row + 1)
+
+        d1 = row - c + (n - 1)
+        d2 = row + c
+
+        if used_col[c] or used_diag1[d1] or used_diag2[d2]:
+            continue
+
+        used_col[c] = True
+        used_diag1[d1] = True
+        used_diag2[d2] = True
+
+        backtrack(row + 1)
+
+        used_col[c] = False
+        used_diag1[d1] = False
+        used_diag2[d2] = False
 
 
 backtrack(0)
