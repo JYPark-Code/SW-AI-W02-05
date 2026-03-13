b4444fbd7b756ff63025c50d0bfa129ff1bf1161
jypark <toboy93@gmail.com>
2026-03-13 19:09:18 +0900

    feat: 코드 최적화 O(2n) -> O(n)

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -48,17 +48,18 @@ input = sys.stdin.readline
 N = int(input())
 
 mail_content = []
-reversed_list = []
+# reversed_list = []
 for _ in range(N):
     original_text = input().strip()
     mail_content.append(original_text)
-    reversed_list.append(original_text[::-1])
+    reversed_ = original_text[::-1]
 
-for reversed_ in reversed_list:
     if reversed_ in mail_content:
         mid = len(reversed_) // 2
         print(len(reversed_), reversed_[mid])
         break
+
+
 
 # print(mail_content)
 # print(reversed_list)
