418d9a6b3ffec535c6b8cfa0b6daa9edc7cd573f
jypark <toboy93@gmail.com>
2026-03-13 17:00:12 +0900

    feat: 이분 탐색으로 풀어서 제출하기

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,60 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+"""
+문제
+N개의 정수 A[1], A[2], …, A[N]이 주어져 있을 때, 이 안에 X라는 정수가 존재하는지 알아내는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 자연수 N(1 ≤ N ≤ 100,000)이 주어진다.
+다음 줄에는 N개의 정수 A[1], A[2], …, A[N]이 주어진다.
+다음 줄에는 M(1 ≤ M ≤ 100,000)이 주어진다.
+다음 줄에는 M개의 수들이 주어지는데, 이 수들이 A안에 존재하는지 알아내면 된다.
+모든 정수의 범위는 -231 보다 크거나 같고 231보다 작다.
+
+출력
+M개의 줄에 답을 출력한다. 존재하면 1을, 존재하지 않으면 0을 출력한다.
+
+예제 입력 1
+5
+4 1 5 2 3
+5
+1 3 7 9 5
+
+예제 출력 1
+1
+1
+0
+0
+1
+
+"""
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+A_list = sorted(map(int, input().split()))
+
+M = int(input().strip())
+B_list = list(map(int, input().split()))
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return 1
+
+        if arr[mid] < target:
+            left = mid + 1
+
+        elif arr[mid] > target:
+            right = mid - 1
+
+    return 0
+
+for b in B_list:
+    print(binary_search(A_list, b))
+
