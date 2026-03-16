aaa22574cc1062cb8886280373395642b4435e25
jypark <toboy93@gmail.com>
2026-03-16 16:48:50 +0900

    feat: 시작 지점은 둘 다 헤드. 증가할때만 1칸 2칸씩.

--- week3/problem-solving/난이도하_링크드리스트의중앙_리트코드.py
+++ week3/problem-solving/난이도하_링크드리스트의중앙_리트코드.py
@@ -8,10 +8,10 @@ class ListNode:
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow = head
-        fast = head.next.next
+        fast = head
 
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next
+            fast = fast.next.next
 
         return slow
