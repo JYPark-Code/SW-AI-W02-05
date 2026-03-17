1c56ea30cb99bdb5b6e579e44cbef66827c66be3
jypark <toboy93@gmail.com>
2026-03-17 11:27:07 +0900

    feat: 중복 찾기 set, list 길이 차이로 구하기

---
+++ week3/problem-solving/난이도하_중복찾기_리트코드.py
@@ -0,0 +1,10 @@
+# 링크 : https://leetcode.com/problems/contains-duplicate/description/
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        set_nums = set(nums)
+
+        if len(nums) == len(set_nums):
+            return False
+
+        else:
+            return True
