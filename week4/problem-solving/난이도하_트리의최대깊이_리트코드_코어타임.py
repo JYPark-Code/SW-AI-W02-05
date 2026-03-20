bfeca8d3a79c51f740d01b3c76c542efb5ff8105
jypark <toboy93@gmail.com>
2026-03-20 20:04:11 +0900

    feat: 왼쪽과 오른쪽을 비교해서 재귀를 사용해서 깊이를 찾음

---
+++ week4/problem-solving/난이도하_트리의최대깊이_리트코드_코어타임.py
@@ -0,0 +1,21 @@
+# 3월 20일 코어타임
+# 링크 : https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left, right)
