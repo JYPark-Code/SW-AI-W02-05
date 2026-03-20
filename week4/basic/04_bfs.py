0b6c15bea75cb794fff15bca762311e509116185
jypark <toboy93@gmail.com>
2026-03-20 19:49:44 +0900

    feat: 원래 리스트로 해도 되지만 안정적으로 하기 위해서 set()가 더 낫다고 판단되어서 코드를 변경함.

--- week4/basic/04_bfs.py
+++ week4/basic/04_bfs.py
@@ -43,19 +43,27 @@ def bfs(graph, start):
     Returns:
         방문 순서 리스트
     """
-    visited = []
-    
+    visited = set()
+
+
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    queue = deque([start])
+    visited.add(start)
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
-    
-    return visited
+    while queue:
+        v = queue.popleft()
+
+        for i in graph[v]:
+            if i not in visited:
+                queue.append(i)
+                visited.add(i)
+
+    return list(visited)
 
 # 테스트 케이스
 if __name__ == "__main__":
