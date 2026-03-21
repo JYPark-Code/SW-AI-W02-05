5e2a7c38f3dea490de87385fb0576fadfd071c90
jypark <toboy93@gmail.com>
2026-03-21 19:39:48 +0900

    feat: BFS로 최대 깊이 알아내기. 방법 추가

--- week4/problem-solving/난이도하_트리의최대깊이_리트코드_코어타임.py
+++ week4/problem-solving/난이도하_트리의최대깊이_리트코드_코어타임.py
@@ -1,7 +1,7 @@
 # 3월 20일 코어타임
 # 링크 : https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
 from typing import Optional
-
+from collections import deque
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -9,8 +9,9 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
-class Solution:
+
+class DFS_Solution:
+    # DFS
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
@@ -19,3 +20,29 @@ class Solution:
         right = self.maxDepth(root.right)
 
         return 1 + max(left, right)
+
+
+
+class BFS_Solution:
+    # BFS
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+
+        queue = deque([root])
+        depth = 0
+
+        while queue:
+            level_size = len(queue)
+
+            for _ in range(level_size):
+                node = queue.popleft()
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            depth += 1
+
+        return depth
