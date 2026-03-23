921cca69d073523c039c5765d15c0dde3f2c9571
jypark <toboy93@gmail.com>
2026-03-23 17:48:16 +0900

    feat: BFS가 아닌 DP로 풀었음.

--- week4/problem-solving/난이도중_BFS_동전2_골드5.py
+++ week4/problem-solving/난이도중_BFS_동전2_골드5.py
@@ -1,2 +1,39 @@
 # BFS - 동전 2 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2294
+"""
+문제
+n가지 종류의 동전이 있다. 이 동전들을 적당히 사용해서, 그 가치의 합이 k원이 되도록 하고 싶다.
+그러면서 동전의 개수가 최소가 되도록 하려고 한다. 각각의 동전은 몇 개라도 사용할 수 있다.
+
+입력
+첫째 줄에 n, k가 주어진다. (1 ≤ n ≤ 100, 1 ≤ k ≤ 10,000) 다음 n개의 줄에는 각각의 동전의 가치가 주어진다.
+동전의 가치는 100,000보다 작거나 같은 자연수이다. 가치가 같은 동전이 여러 번 주어질 수도 있다.
+
+출력
+첫째 줄에 사용한 동전의 최소 개수를 출력한다. 불가능한 경우에는 -1을 출력한다.
+
+"""
+import sys
+input = sys.stdin.readline
+
+# DP 테이블이 좀 더 맞는 문제
+MAX = float('inf')
+
+n, k = map(int, input().split())
+
+coins = []
+dp = [MAX] * (k + 1)
+dp[0] = 0
+
+for _ in range(n):
+    coins.append(int(input().strip()))
+
+for i in range(k + 1):
+    for c in coins:
+        if i - c >= 0:
+            dp[i] = min(dp[i], dp[i - c] + 1)
+
+if dp[k] == MAX:
+    print(-1)
+else:
+    print(dp[k])
