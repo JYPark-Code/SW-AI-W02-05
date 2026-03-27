78adbf53831f1df716807922c75f614632407f5b
jypark <toboy93@gmail.com>
2026-03-27 21:35:47 +0900

    feat: 로직은 맞아도 n이 3보다 작을 때 케이스 챙기기.

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,29 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+if n == 0:
+    print(0)
+    exit()
+if n == 1:
+    print(1)
+    exit()
+if n == 2:
+    print(1)
+    exit()
+
+if n >= 3:
+    dp = [0] * (n+1)
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
+
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
